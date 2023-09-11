527e19c9e0d4b9a5bd7b13e11f9c070f4ac988d2
a20140085 <131030671+a20140085@users.noreply.github.com>
2023-09-11 13:02:09 -0500

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,22 @@
+alert("Bienvenido al restuarante. Esta es tu cuenta.")
+//Ingresar Datos
+var num1 = parseInt(prompt("El costo de la entrada es"));
+var num2 = parseInt(prompt("Es costo del segundo es"));
+var num3 = parseInt(prompt("El costo del postre es"));
+
+
+
+
+//Procesar Datos
+var suma = num1 + num2;
+var suma = num2 + num3;
+var suma = num3 + num4;
+var total = num1 + num2 + num3;
+
+
+
+//Mostrar Datos
+console.log("El costo de la entrada es: " + num1);
+console.log("El costo del segundo es " + num2);
+console.log("El costo del postre es " + num3);
+
